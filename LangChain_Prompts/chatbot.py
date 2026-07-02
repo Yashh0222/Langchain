7318eafa8663210bc02033a5bc8fa0aca7ddd2c0
Yash Koparkar <koparkaryash41@gmail.com>
2026-07-02 14:27:34 +0530

    Initial commit1

---
+++ LangChain_Prompts/chatbot.py
@@ -0,0 +1,22 @@
+from langchain_groq import ChatGroq
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGroq(model="llama-3.3-70b-versatile")
+
+#chat_history is used to remember chat between agent and user without chathistory it cant remember what was there chat 
+chat_history = [
+    SystemMessage(content='Ypu are a Helpful Assistent')
+]
+
+while True:
+    user_input = input('You: ')
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == 'exit':
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print('AI: ', result.content)
+print(chat_history)
