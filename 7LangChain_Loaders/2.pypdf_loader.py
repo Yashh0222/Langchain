2de4d04886825fa4679b9ff86850a3770507ed48
Yash Koparkar <koparkaryash41@gmail.com>
2026-07-10 19:31:38 +0530

    Updated Loaders

--- 7LangChain_Loaders/2.pypdf_loader.py
+++ 7LangChain_Loaders/2.pypdf_loader.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 
-#used to load pdf and after loading prpdf loader converts it into list where each page is seperated 
+#used to load pdf and after loading pypdf loader converts it into list where each page is seperated 
 loader = PyPDFLoader('dl-curriculum.pdf')
 
 docs = loader.load()
